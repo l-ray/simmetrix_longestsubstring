078091223fd86d434203c5e93101363bf64fdee6
L-Ray <lennart_reuther@web.de>
2014-01-17 19:12:01 +0000

    starts using module structure

--- test/simmetrix.longestsubstring.js
+++ test/simmetrix.longestsubstring.js
@@ -1,15 +1,15 @@
-require('../lib/simmetrix.longestsubstring')
+var longestSubstring = require('../lib/simmetrix.longestsubstring')
 var expect = require('expect.js');
 
 describe('Simmetrix.LongestSubstring', function(){
   describe('#similarity()', function(){
     it('should return a proper similarity between 0 and 1', function(){
-      expect(Simmetrix.LongestSubstring.similarity("Test String1", "Test String2").toFixed(4)).to.be.equal('0.9167');
+      expect(longestSubstring.similarity("Test String1", "Test String2").toFixed(4)).to.be.equal('0.9167');
     }),
     it('should return 0 with undefined or empty strings', function(){
-      expect(Simmetrix.LongestSubstring.similarity("", "Test String2").toFixed(4)).to.be.equal('0.0000');
-      expect(Simmetrix.LongestSubstring.similarity(undefined, "Test String2").toFixed(4)).to.be.equal('0.0000');
-      expect(Simmetrix.LongestSubstring.similarity("Test String1", undefined).toFixed(4)).to.be.equal('0.0000');
+      expect(longestSubstring.similarity("", "Test String2").toFixed(4)).to.be.equal('0.0000');
+      expect(longestSubstring.similarity(undefined, "Test String2").toFixed(4)).to.be.equal('0.0000');
+      expect(longestSubstring.similarity("Test String1", undefined).toFixed(4)).to.be.equal('0.0000');
     })
   })
 })
